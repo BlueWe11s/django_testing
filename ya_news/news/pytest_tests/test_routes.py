f6a34ad5f908fbab7ac9796fa2fcbcc4e91e488c
BlueWe11s <lshepetr@gmail.com>
2024-10-03 17:59:57 +0300

    7

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -9,23 +9,16 @@ from pytest_django.asserts import assertRedirects
 
 ADMIN_CLIENT = lazy_fixture('admin_client')
 AUTHOR_CLIENT = lazy_fixture('author_client')
-NEWS_DELETE_URL = 'news:delete'
-NEWS_DETAIL_URL = 'news:detail'
-NEWS_EDIT_URL = 'news:edit'
-NEWS_HOME_URL = 'news:home'
-USERS_LOGIN_URL = 'users:login'
-USERS_LOGOUT_URL = 'users:logout'
-USERS_SIGNUP_URL = 'users:signup'
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
     'name, note_object',
     (
-        (NEWS_HOME_URL, None),
-        (USERS_LOGIN_URL, None),
-        (USERS_LOGOUT_URL, None),
-        (USERS_SIGNUP_URL, None),
+        ('news:home', None),
+        ('users:login', None),
+        ('users:logout', None),
+        ('users:signup', None),
     ),
 )
 def test_pages_availability_anonymous_user(name, note_object, client):
@@ -43,7 +36,7 @@ def test_pages_availability_anonymous_user(name, note_object, client):
 )
 @pytest.mark.parametrize(
     'name',
-    (NEWS_EDIT_URL, NEWS_DELETE_URL),
+    ('news:edit', 'news:delete'),
 )
 def test_availability_for_comment_edit_and_delete(
         parametrized_client, name, comment, expected_status
@@ -55,10 +48,10 @@ def test_availability_for_comment_edit_and_delete(
 
 @pytest.mark.parametrize(
     'name',
-    (NEWS_EDIT_URL, NEWS_DELETE_URL),
+    ('news:edit', 'news:delete'),
 )
 def test_redirects(client, name, comment):
-    login_url = reverse(USERS_LOGIN_URL)
+    login_url = reverse('users:login')
     url = reverse(name, args=(comment.id,))
     expected_url = f'{login_url}?next={url}'
     response = client.get(url)
